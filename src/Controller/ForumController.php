b411e371fa5fcf26f76925809d7f4e8077bd2b9a
julienDev76-75 <julien.coart@gmail.com>
2021-06-28 17:10:20 +0200

    second commit

--- src/Controller/ForumController.php
+++ src/Controller/ForumController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Subject;
 
 class ForumController extends AbstractController
 {
@@ -13,8 +14,18 @@ class ForumController extends AbstractController
     //on peut faire deux routes pour renvoyer la même page
     public function index(): Response
     {
+        //va me chercher le manager de la classe Subject qu'on stock dans $subjectRepository pour indiquer la table (manière de procéder)
+        //https://symfony.com/doc/current/profiler.html pour avoir la toolbar de debug et voir les infos de larray
+        //on va maper le resultat dans index.html.twig
+        $subjectRepository = $this->getDoctrine()->getRepository(Subject::class);
+        $subjects = $subjectRepository->findAll();
+        dump($subjects);
+        //comment faire remonter le $exemple dans le twig ? passe le dans le render
+        $exemple = "coucou";
+        //c'est le subjects variable de mon template, j'injecte dans index.html.twig une variable subject qui a pour valeur la variable subject de mon controller
         return $this->render('forum/index.html.twig', [
-            'controller_name' => 'ForumController',
+            'subjects' => $subjects,
+            'coucoudanstwig' => $exemple,
         ]);
     }
 
